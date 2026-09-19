b0cf25b92a6c362a124497f3ee0bc04d3587e68e
smitnk <sumitnaikna29@gmail.com>
2026-09-19 13:55:31 +0530

    Fix native Android project settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,23 +1,12 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        google()
         mavenCentral()
         gradlePluginPortal()
     }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
+    repositories { google(); mavenCentral() }
 }
-
 rootProject.name = "MotionCanvas"
-include(":app")
